9580fbea9ee963e96467fe5859ee7a3e785bd960
paul0pv <maurossdend@gmail.com>
2023-10-19 22:37:44 -0500

    Rewriting

--- react-tip-calculator/src/components/Title/index.jsx
+++
@@ -1,4 +0,0 @@
-/* eslint-disable react/prop-types */
-export default function Title({ text }) {
-    return <h2 className="font-semibold text-green-900 my-3">{text}</h2>;
-  }
--- semana1/README.md
+++
@@ -1,1 +0,0 @@
-# Mi pagina web
