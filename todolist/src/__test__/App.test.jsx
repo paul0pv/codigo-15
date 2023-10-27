5e410a03875ae649b7f7e0cf7b965cf3cc77f62d
paul0pv <maurossdend@gmail.com>
2023-10-26 23:00:54 -0500

    Testing p1

---
+++ todolist/src/__test__/App.test.jsx
@@ -0,0 +1,34 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import App from "../App";
+
+describe("Render App component", () => {
+    it("Test App flow", () => {
+        render(<App />);
+        const title = screen.getByText("Crear cuenta");
+        const button = screen.getByLabelText("Crear cuenta");
+        expect(title).toBeInTheDocument();
+        fireEvent.click(button);
+        const errors = screen.getAllByText("Este campo es requerido");
+        expect(errors).toHaveLength(4);
+    });
+
+    it("Test SignUp", () => {
+        const response = {
+            json: vi.fn().mockResolvedValue(mockUserResponse),
+        }
+
+        global.fetch = vi.fn().mockResolvedValue(response);
+
+        render(<App />);
+        const inputName = screen.getByLabelText("name");
+        const inputLastame = screen.getByLabelText("Lastname");
+        const inputEmail = screen.getByLabelText("email");
+        const inputPassword = screen.getByLabelText("password");
+    
+        fireEvent.change(inputName, {target: { value: "Pepe"}});
+        fireEvent.change(inputLastame, {target:{}});
+        fireEvent.change(inputEmail, {target:{}});
+        fireEvent.change(inputPassword, {target:{}});
+    })
+})
--- todolist/vite.config.js
+++ todolist/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/__test__/setup.js",
+    globals: true,
+  },
 })
